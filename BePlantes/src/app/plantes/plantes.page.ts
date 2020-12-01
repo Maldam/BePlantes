82abccd82159db8b76d5865a6c0d82947c9a0852
Maldam <damien.malice@hotmail.com>
2020-12-01 16:10:16 +0100

    Remplacement tab1 > plantes

---
+++ BePlantes/src/app/plantes/plantes.page.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-plantes',
+  templateUrl: 'plantes.page.html',
+  styleUrls: ['plantes.page.scss']
+})
+export class PlantesPage {
+
+  constructor() {}
+
+}
--- BePlantes/src/app/tabs/tabs-routing.module.ts
+++ BePlantes/src/app/tabs/tabs-routing.module.ts
@@ -8,8 +8,8 @@ const routes: Routes = [
     component: TabsPage,
     children: [
       {
-        path: 'tab1',
-        loadChildren: () => import('../tab1/tab1.module').then(m => m.Tab1PageModule)
+        path: 'plantes',
+        loadChildren: () => import('../plantes/plantes.module').then(m => m.PlantesPageModule)
       },
       {
         path: 'tab2',
@@ -21,14 +21,14 @@ const routes: Routes = [
       },
       {
         path: '',
-        redirectTo: '/tabs/tab1',
+        redirectTo: '/tabs/plantes',
         pathMatch: 'full'
       }
     ]
   },
   {
     path: '',
-    redirectTo: '/tabs/tab1',
+    redirectTo: '/tabs/plantes',
     pathMatch: 'full'
   }
 ];
