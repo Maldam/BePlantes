3ceed0dcad36ac8a9cc6de791d77b0b7d0c83874
Maldam <damien.malice@hotmail.com>
2020-12-02 18:50:45 +0100

    Création modele plante

---
+++ BePlantes/src/app/modeles/plante.modele.ts
@@ -0,0 +1,20 @@
+export class PlanteModele {
+    public key: string;
+    public imageURL: string = "";
+    public nom: string = "";
+    public couleur: string = "";
+    public temperatureMinimum;
+    public temperatureMaximum;
+    public terrain: string = "";
+    public bouturage: string = "";
+    public semis: string = "";
+    public floraison;
+    public exposition;
+    public vivace;
+    public description;
+    public taille;
+    public mellifere;
+    public notes: string = "";
+    constructor(
+    ) { }
+}
