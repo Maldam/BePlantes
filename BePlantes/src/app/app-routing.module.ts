2cef72fe2d2a6b2381012297e1c073e1fc2b7d85
Maldam <damien.malice@hotmail.com>
2020-12-14 00:41:09 +0100

    Adaptation code de module app-routing

--- BePlantes/src/app/app-routing.module.ts
+++ BePlantes/src/app/app-routing.module.ts
@@ -1,15 +1,19 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
-
+import { GardeConnexionService } from './services/garde-connexion.service';
 const routes: Routes = [
   {
-    path: '',
+    path: 'connexion',
+    loadChildren: () => import('./connexion/connexion.module').then( m => m.ConnexionPageModule)
+  },
+  {
+    path: '',canActivate: [GardeConnexionService],
     loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
-  },  {
-    path: 'ajouter-plante',
+  },
+  {
+    path: 'ajouter-plante',canActivate: [GardeConnexionService],
     loadChildren: () => import('./ajouter-plante/ajouter-plante.module').then( m => m.AjouterPlantePageModule)
   }
-
 ];
 @NgModule({
   imports: [
--- BePlantes/src/app/app.module.ts
+++ BePlantes/src/app/app.module.ts
@@ -8,14 +8,45 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ConnexionService } from './services/connexion.service';
+import { PlantesService } from './services/plantes.service';
+import { AngularFireModule } from '@angular/fire';
+import { AngularFireDatabaseModule } from '@angular/fire/database';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+import { AngularFireStorageModule } from '@angular/fire/storage';
+import { GardeConnexionService } from './services/garde-connexion.service';
+import { firebaseConfig } from './app.firebase.config';
+import { Camera } from '@ionic-native/camera/ngx';
+
+// var firebaseConfig = {
+//   apiKey: "AIzaSyD7kDm6lgSMS07e3y4AMTBBmRXtw_KkWPQ",
+//   authDomain: "beplantes.firebaseapp.com",
+//   databaseURL: "https://beplantes-default-rtdb.europe-west1.firebasedatabase.app",
+//   projectId: "beplantes",
+//   storageBucket: "beplantes.appspot.com",
+//   messagingSenderId: "473724761524",
+//   appId: "1:473724761524:web:b74a8997454c77eda76ee1",
+//   measurementId: "G-SL6YPQCRE4"
+// };
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [BrowserModule, 
+    IonicModule.forRoot(), 
+    AppRoutingModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireDatabaseModule,
+    AngularFireAuthModule,
+    AngularFireStorageModule
+  ],
   providers: [
     StatusBar,
+    Camera,
     SplashScreen,
+    ConnexionService,
+    GardeConnexionService,
+    PlantesService,
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
   ],
   bootstrap: [AppComponent]
