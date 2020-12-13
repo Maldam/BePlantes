929801f5492ccd2d02954c5940234a27a3d22f60
Maldam <damien.malice@hotmail.com>
2020-12-14 00:42:02 +0100

    Mise en place config firebase

---
+++ BePlantes/src/app/app.firebase.config.ts
@@ -0,0 +1,10 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyD7kDm6lgSMS07e3y4AMTBBmRXtw_KkWPQ",
+    authDomain: "beplantes.firebaseapp.com",
+    databaseURL: "https://beplantes-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "beplantes",
+    storageBucket: "beplantes.appspot.com",
+    messagingSenderId: "473724761524",
+    appId: "1:473724761524:web:b74a8997454c77eda76ee1",
+    measurementId: "G-SL6YPQCRE4"
+  };
