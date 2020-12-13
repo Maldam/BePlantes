17d1d6777276f5ade053c6d9d106b725272147b3
Maldam <damien.malice@hotmail.com>
2020-12-14 00:41:33 +0100

    Modification app component

--- BePlantes/src/app/app.component.ts
+++ BePlantes/src/app/app.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
-
 import { Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { Router } from '@angular/router';
+import { AngularFireAuth } from '@angular/fire/auth';
 
 @Component({
   selector: 'app-root',
@@ -11,15 +12,25 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 })
 export class AppComponent {
   constructor(
+    private router: Router,
+    public angularFireAuth: AngularFireAuth,
     private platform: Platform,
     private splashScreen: SplashScreen,
     private statusBar: StatusBar
   ) {
     this.initializeApp();
   }
-
   initializeApp() {
     this.platform.ready().then(() => {
+      this.angularFireAuth.authState.subscribe(auth => {
+        if (!auth) {
+          console.log('non connecté');
+          this.router.navigateByUrl('/connexion');
+        } else {
+          this.router.navigateByUrl('/');
+          console.log('Connecté: ' + auth.uid);
+        }
+      });
       this.statusBar.styleDefault();
       this.splashScreen.hide();
     });
