4cffedbdc1d2a40901c03c4a715bb7465768794b
Maldam <damien.malice@hotmail.com>
2020-12-14 00:37:25 +0100

    Adaptation page ajout-plante

--- BePlantes/src/app/ajouter-plante/ajouter-plante.page.ts
+++ BePlantes/src/app/ajouter-plante/ajouter-plante.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Camera, CameraOptions } from '@ionic-native/camera/ngx';
+import { PlanteModele } from '../modeles/plante.modele';
 
 @Component({
   selector: 'app-ajouter-plante',
@@ -6,9 +8,38 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./ajouter-plante.page.scss'],
 })
 export class AjouterPlantePage implements OnInit {
+  private plante: PlanteModele = new PlanteModele; 
 
-  constructor() { }
+  constructor(
+    private camera: Camera,
+    //= new PlanteModele()
 
+  ) { }
+
+  private async openLibrary() {
+    const options: CameraOptions = {
+      quality: 100,
+      destinationType: this.camera.DestinationType.DATA_URL,
+      encodingType: this.camera.EncodingType.JPEG,
+      mediaType: this.camera.MediaType.PICTURE,
+      targetWidth: 1000,
+      targetHeight: 1000,
+      sourceType: this.camera.PictureSourceType.PHOTOLIBRARY
+    };
+    return await this.camera.getPicture(options);
+  }
+  private async openCamera() {
+    const options: CameraOptions = {
+      quality: 100,
+      destinationType: this.camera.DestinationType.DATA_URL,
+      encodingType: this.camera.EncodingType.JPEG,
+      mediaType: this.camera.MediaType.PICTURE,
+      targetWidth: 1000,
+      targetHeight: 1000,
+      sourceType: this.camera.PictureSourceType.CAMERA
+    };
+    return await this.camera.getPicture(options);
+  }
   ngOnInit() {
   }
 
