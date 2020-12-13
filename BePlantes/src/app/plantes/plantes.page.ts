9d201454d9b5407f941337865ec5a14226d57447
Maldam <damien.malice@hotmail.com>
2020-12-14 00:38:01 +0100

    Modification page plantes

--- BePlantes/src/app/plantes/plantes.page.ts
+++ BePlantes/src/app/plantes/plantes.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { PlanteModele } from '../modeles/plante.modele';
+
 
 @Component({
   selector: 'app-plantes',
@@ -6,6 +8,7 @@ import { Component } from '@angular/core';
   styleUrls: ['plantes.page.scss']
 })
 export class PlantesPage {
+  public listePlantes: Array<PlanteModele> = new Array<PlanteModele>();
 
   constructor() {}
 
