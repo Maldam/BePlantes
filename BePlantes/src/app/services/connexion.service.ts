788303132fe253a2ebf33b07bb8cb05a6f791c70
Maldam <damien.malice@hotmail.com>
2020-12-14 00:39:11 +0100

    Ajout service connexion

---
+++ BePlantes/src/app/services/connexion.service.ts
@@ -0,0 +1,36 @@
+import { Injectable } from '@angular/core';
+import firebase from 'firebase/app'
+@Injectable()
+export class ConnexionService {
+  public creerUtilisateur(email: string, password: string) {
+    return new Promise(
+      (resolve, reject) => {
+        firebase.auth().createUserWithEmailAndPassword(email, password).then(
+          () => {
+            resolve('');
+          },
+          (error) => {
+            reject(error);
+          }
+        );
+      }
+    );
+  }
+  public connexionUtilisateur(email: string, password: string) {
+    return new Promise(
+      (resolve, reject) => {
+        firebase.auth().signInWithEmailAndPassword(email, password).then(
+          () => {
+            resolve('');
+          },
+          (error) => {
+            reject(error);
+          }
+        );
+      }
+    );
+  }
+  public deconnexionUtilisateur() {
+    firebase.auth().signOut();
+  }
+}
