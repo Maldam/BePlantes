c360ed91404b6af5b72564c0c8134fe4cb8e0175
Maldam <damien.malice@hotmail.com>
2020-12-20 22:15:41 +0100

    Modification du modele plante

--- BePlantes/src/app/modeles/plante.modele.ts
+++ BePlantes/src/app/modeles/plante.modele.ts
@@ -1,19 +1,21 @@
 export class PlanteModele {
     public key: string;
     public imageURL: string = "";
-    public nom: string = "";
+    public genre: string = "";
+    public variete: string = "";
+    public surnom: string = "";
     public couleur: string = "";
-    public temperatureMinimum;
-    public temperatureMaximum;
+    public temperatureMinimum: string = "";
+    public temperatureMaximum: string = "";
     public terrain: string = "";
     public bouturage: string = "";
     public semis: string = "";
-    public floraison;
-    public exposition;
-    public vivace;
-    public description;
-    public taille;
-    public mellifere;
+    public floraison: string = "";
+    public exposition: string = "";
+    public vivace: string = "";
+    public description: string = "";
+    public taille: string = "";
+    public mellifere: string = "";
     public notes: string = "";
     constructor(
     ) { }
