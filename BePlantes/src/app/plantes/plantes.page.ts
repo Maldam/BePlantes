2de4756a4750b87a44f468d9fa92d4b97ce9cd0e
Maldam <damien.malice@hotmail.com>
2020-12-20 22:17:55 +0100

    Adaptation page plante

--- BePlantes/src/app/plantes/plantes.page.ts
+++ BePlantes/src/app/plantes/plantes.page.ts
@@ -1,15 +1,20 @@
 import { Component } from '@angular/core';
 import { PlanteModele } from '../modeles/plante.modele';
-
-
+import { PlantesService } from '../services/plantes.service';
 @Component({
   selector: 'app-plantes',
   templateUrl: 'plantes.page.html',
   styleUrls: ['plantes.page.scss']
 })
 export class PlantesPage {
-  public listePlantes: Array<PlanteModele> = new Array<PlanteModele>();
-
-  constructor() {}
-
+  //public plantes: Array<PlanteModele> = new Array<PlanteModele>();
+  public listeGenres: any;
+  constructor(
+    public plantesService: PlantesService,
+  ) {
+    this.plantesService.getGenres().subscribe(genres => {
+      //this.plantes = plantes;
+      this.listeGenres = genres;
+    });
+  }
 }
